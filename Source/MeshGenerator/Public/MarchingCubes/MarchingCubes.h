186fa00b147f9e500c8cd73d37abc5472b6cb5b8
Dan Raquel <draquel@webjynx.com>
2024-12-15 00:38:54 -0500

    added marchingcubessettings object

--- Source/MeshGenerator/Public/MarchingCubes/MarchingCubes.h
+++ Source/MeshGenerator/Public/MarchingCubes/MarchingCubes.h
@@ -4,6 +4,38 @@
 #include "UObject/Object.h"
 #include "MarchingCubes.generated.h"
 
+USTRUCT(BlueprintType)
+struct FMarchingCubesSettings
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float iso;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool interpolate;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool renderSides;
+	
+public:
+	
+	FMarchingCubesSettings()
+	{
+		iso = 0.5f;
+		interpolate = false;
+		renderSides = false;
+	}
+
+	FMarchingCubesSettings(float _iso, bool _interpolate, bool _renderSides)
+	{
+		iso = _iso;
+		interpolate = _interpolate;
+		renderSides = _renderSides;
+	}
+	
+};
+
 UCLASS()
 class MESHGENERATOR_API UMarchingCubes : public UObject
 {
